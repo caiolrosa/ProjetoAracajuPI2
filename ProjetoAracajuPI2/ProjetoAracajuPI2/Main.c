ac27620ce7b604b5f90af07dc12e345c22ccc79b
Caio Rosa <caiolrosa1912@gmail.com>
2016-09-25 01:23:35 -0300

    Objetos base e restante de codigo allegro
    Inclui alguns comentarios também

--- ProjetoAracajuPI2/ProjetoAracajuPI2/Main.c
+++ ProjetoAracajuPI2/ProjetoAracajuPI2/Main.c
@@ -1,6 +1,12 @@
+#pragma region Include
+
 #include <stdio.h>
 #include <allegro5\allegro.h>
+#include <allegro5\allegro_font.h>
+#include <allegro5\allegro_ttf.h>
 #include "Objects.h"
+
+#pragma endregion
 
 // Variaveis globais
 const int width = 800;
@@ -15,7 +21,7 @@ int main() {
 	const int FPS = 60;
 
 	// Variaveis de objeto
-	
+
 
 	// Variaveis do Allegro
 	ALLEGRO_DISPLAY *display = NULL;
@@ -29,7 +35,20 @@ int main() {
 	}
 
 	// Initialize display
-	display = al_create_display(width, height);
+	display = al_create_display(width, height);					// Inicializa o display
+
+	// Addons e instalações do allegro
+	al_install_mouse();											// Possibilita o uso do mouse
+	//al_init_font_addon();										// Possibilita escrever na tela
+	//al_init_ttf_addon();										// Possibilita usar formato ttf
+
+	event_queue = al_create_event_queue();						// Cria "lista" de eventos
+	timer = al_create_timer(1 / FPS);							// Inicializa o timer para que tenhamos 60 fps
+
+	// Registro de eventos para a lista de eventos
+	al_register_event_source(event_queue, al_get_mouse_event_source());			// Registra o mouse na lista de eventos
+	al_register_event_source(event_queue, al_get_timer_event_source(timer));			// Registra o timer na lista de eventos
+	al_register_event_source(event_queue, al_get_display_event_source(display));			// Registra o display na lista de eventos
 
 	// Caso nao inicializar o display retorna -1 para o SO
 	if (!display)
@@ -39,6 +58,21 @@ int main() {
 
 	// Looping Principal
 	while (!finished) {
-		// ...
+		ALLEGRO_EVENT ev;
+		al_wait_for_event(event_queue, &ev);
+
+		// É invocado a cada frame "Função de update" 
+		if (ev.type == ALLEGRO_EVENT_TIMER)
+		{
+
+		}
+
+		// Permite fechar a janela pelo X
+		if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+		{
+			finished = true;
+		}
 	}
 }
+
+// Definicao de funcoes
--- ProjetoAracajuPI2/ProjetoAracajuPI2/Objects.h
+++ ProjetoAracajuPI2/ProjetoAracajuPI2/Objects.h
@@ -1,1 +1,214 @@
-// Structs...
+typedef struct Jogador {
+	int score;
+	int vidas;
+} Jogador;
+
+#pragma region Regiao Sul
+
+// Estados regiao Sul
+typedef struct RioGrandeDoSul {
+	char *nome;
+	char *capital;
+	int boundX;
+	int boundY;
+} RioGrandeDoSul;
+
+typedef struct SantaCatarina {
+	char *nome;
+	char *capital;
+	int boundX;
+	int boundY;
+} SantaCatarina;
+
+typedef struct Parana {
+	char *nome;
+	char *capital;
+	int boundX;
+	int boundY;
+} Parana;
+
+#pragma endregion
+#pragma region Regiao Sudeste
+
+// Estados regiao Sudeste
+typedef struct SaoPaulo {
+	char *nome;
+	char *capital;
+	int boundX;
+	int boundY;
+} SaoPaulo;
+
+typedef struct RioDeJaneiro {
+	char *nome;
+	char *capital;
+	int boundX;
+	int boundY;
+} RioDeJaneiro;
+
+typedef struct EspiritoSanto {
+	char *nome;
+	char *capital;
+	int boundX;
+	int boundY;
+} EspiritoSanto;
+
+typedef struct MinasGerais {
+	char *nome;
+	char *capital;
+	int boundX;
+	int boundY;
+} MinasGerais;
+
+#pragma endregion
+#pragma region Regiao Centro-Oeste
+
+// Estados regiao Centro-Oeste
+typedef struct MatoGrossoDoSul {
+	char *nome;
+	char *capital;
+	int boundX;
+	int boundY;
+} MatoGrossoDoSul;
+
+typedef struct Goias {
+	char *nome;
+	char *capital;
+	int boundX;
+	int boundY;
+} Goias;
+
+typedef struct MatoGrosso {
+	char *nome;
+	char *capital;
+	int boundX;
+	int boundY;
+} MatoGrosso;
+
+typedef struct DistritoFederal {
+	char *nome;
+	char *capital;
+	int boundX;
+	int boundY;
+} DistritoFederal;
+
+#pragma endregion
+#pragma region Regiao Nordeste
+
+// Estados regiao Nordeste
+typedef struct Bahia {
+	char *nome;
+	char *capital;
+	int boundX;
+	int boundY;
+} Bahia;
+
+typedef struct Sergipe {
+	char *nome;
+	char *capital;
+	int boundX;
+	int boundY;
+} Sergipe;
+
+typedef struct Alagoas {
+	char *nome;
+	char *capital;
+	int boundX;
+	int boundY;
+} Alagoas;
+
+typedef struct Pernambuco {
+	char *nome;
+	char *capital;
+	int boundX;
+	int boundY;
+} Pernambuco;
+
+typedef struct Paraiba {
+	char *nome;
+	char *capital;
+	int boundX;
+	int boundY;
+} Paraiba;
+
+typedef struct RioGrandeDoNorte {
+	char *nome;
+	char *capital;
+	int boundX;
+	int boundY;
+} RioGrandeDoNorte;
+
+typedef struct Ceara {
+	char *nome;
+	char *capital;
+	int boundX;
+	int boundY;
+} Ceara;
+
+typedef struct Piaui {
+	char *nome;
+	char *capital;
+	int boundX;
+	int boundY;
+} Piaui;
+
+typedef struct Maranhao {
+	char *nome;
+	char *capital;
+	int boundX;
+	int boundY;
+} Maranhao;
+
+#pragma endregion
+#pragma region Regiao Norte
+
+// Estados regiao Norte
+typedef struct Tocantins {
+	char *nome;
+	char *capital;
+	int boundX;
+	int boundY;
+} Tocantins;
+
+typedef struct Para {
+	char *nome;
+	char *capital;
+	int boundX;
+	int boundY;
+} Para;
+
+typedef struct Amapa {
+	char *nome;
+	char *capital;
+	int boundX;
+	int boundY;
+} Amapa;
+
+typedef struct Roraima {
+	char *nome;
+	char *capital;
+	int boundX;
+	int boundY;
+} Roraima;
+
+typedef struct Amazonas {
+	char *nome;
+	char *capital;
+	int boundX;
+	int boundY;
+} Amazonas;
+
+typedef struct Rondonia {
+	char *nome;
+	char *capital;
+	int boundX;
+	int boundY;
+} Rondonia;
+
+typedef struct Acre {
+	char *nome;
+	char *capital;
+	int boundX;
+	int boundY;
+} Acre;
+
+#pragma endregion
